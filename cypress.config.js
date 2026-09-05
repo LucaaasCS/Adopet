22a0b55ea3bb1fa8d4816844b25bc84e1bb3e9b5
Lucas <lucascardoso213@gmail.com>
2026-09-04 21:26:21 -0300

    Implementado relatórios com mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   allowCypressEnv: false,
   video: true,
-
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss', 
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
