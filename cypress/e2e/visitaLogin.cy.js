2f0b465956e866c5f4f1f35696ca4e51deccdc7b
Lucas <lucascardoso213@gmail.com>
2026-09-08 19:56:46 -0300

    Implementação de comandos personalizados no teste VisitaLogin

--- cypress/e2e/visitaLogin.cy.js
+++ cypress/e2e/visitaLogin.cy.js
@@ -6,8 +6,6 @@ describe('Acessa a página de login', () => {
   })
 
   it('Visita a página de login e preenche corretamente os campos', () => {
-    cy.get('[data-test="input-loginEmail"]').type('lucas@example.com');
-    cy.get('[data-test="input-loginPassword"]').type('Secret123!');
-    cy.get('[data-test="submit-button"]').click();
+    cy.login('lucas@example.com', 'Secret123!');
   })
 })
